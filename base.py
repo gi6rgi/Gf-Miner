1724c790ff55128e17e3d6fff153dccaf6f2dd6d
ShredsGang <goshashreeeds@gmail.com>
2020-12-08 20:46:18 +0300

    GraphQl get posts id reversed

--- base.py
+++ base.py
@@ -1,7 +1,7 @@
 # reverse za 300
 # /web/likes/{тут айдишник какой-то хз}/like/
 # User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0
-# ["config"]["ProfilePage"]["user"]["edge_owner_to_timeline_media"]
+# ["data"]["user"]["edge_owner_to_timeline_media"]["edges"]
 
 import requests
 import json
@@ -15,14 +15,16 @@ class Base:
         self.sessionid = sessionid
         self.CSRF = CSRF
 
+    def _get_user_id(self, username: str) -> str:
+        pass
+
     def _get_posts_id(self, username: str) -> list:
-
-        # I will rewrite this part asap.
-        res = requests.get(f'https://instagram.com/{username}', cookies=self.sessionid).text
-        raw_user_data = res.split('<script type="text/javascript">')[-4].split(';</script>')[0].split('window._sharedData = ')[-1]
-        json_user_data = json.loads(raw_user_data)
-        posts_id_data = json_user_data["entry_data"]["ProfilePage"][0]["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"]
-
+        user_id = self._get_user_id(username)
+        url = f'https://www.instagram.com/graphql/query/?query_hash=003056d32c2554def87228bc3fd9668a&variables={{"id":"{user_id}","first":12,"after":""}}'
+        posts_id_raw_data = requests.get(url).text
+        posts_id_json = json.loads(posts_id_raw_data)
+        posts_id_data = posts_id_json["data"]["user"]["edge_owner_to_timeline_media"]["edges"]
+    
         posts_id = []
         for post in posts_id_data:
             posts_id.append(post["node"]["id"])
@@ -36,3 +38,9 @@ class Base:
             for post in posts_id[::step][:posts_to_like]:
                 url = f'https://instagram.com/web/likes/{post}/like/'
                 requests.post(url, headers=self.CSRF, cookies=self.sessionid)
+
+"""
+some tests
+"""
+test = Base(SESSIONID, CSRF)
+test.like_posts(["tihomeowrov"], 5, 1)
