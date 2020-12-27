74e19bfbb4211d2fed046824fa1ba22109a7dd98
ShredsGang <goshashreeeds@gmail.com>
2020-12-27 22:09:06 +0300

    GraphQl get posts id

--- instaclient.py
+++ instaclient.py
@@ -5,14 +5,15 @@ from time import sleep
 
 class InstaClient:
 
-    def __init__(self, sessionid, token):
+    def __init__(self, sessionid, token, user_agent):
         self.sessionid = sessionid
         self.token = token
         self.url = 'https://instagram.com'
+        self.headers = user_agent.update(user_agent)
 
     def _get_user_id(self, username: str) -> str:
         uri = f'{self.url}/web/search/topsearch/?context=blended&query={username}'
-        user_data_raw = requests.get(uri, headers=self.token, cookies=self.sessionid).text
+        user_data_raw = requests.get(uri, headers=self.headers, cookies=self.sessionid).text
         user_data_json = json.loads(user_data_raw)
         
         try:
@@ -25,7 +26,7 @@ class InstaClient:
     def _get_posts_id(self, username: str) -> list:
         user_id = self._get_user_id(username)
         uri = f'{self.url}/graphql/query/?query_hash=003056d32c2554def87228bc3fd9668a&variables={{"id":"{user_id}","first":12}}'
-        posts_data_raw = requests.get(uri, headers=self.token, cookies=self.sessionid).text
+        posts_data_raw = requests.get(uri, headers=self.headers, cookies=self.sessionid).text
         posts_data_json = json.loads(posts_data_raw)
         try:
             posts_data_parsed = posts_data_json["data"]["user"]["edge_owner_to_timeline_media"]["edges"]
@@ -49,7 +50,7 @@ class InstaClient:
 
             for post in posts_id[::step][:posts_to_like]:
                 uri = f'{self.url}/web/likes/{post}/like/'
-                requests.post(uri, headers=self.token, cookies=self.sessionid)
+                requests.post(uri, headers=self.headers, cookies=self.sessionid)
                 sleep(2)
             
-            sleep(3)
+            sleep(5)
